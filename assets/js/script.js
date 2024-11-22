083ed05b415b30439c657318a5351e40e244e3cc
lbachannel <anlb19749@gmail.com>
2024-11-22 20:41:21 +0700

    code side-menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,56 @@ function initJsToggle() {
         };
     });
 }
+
+// menu dropdown
+window.addEventListener("DOMContentLoaded", () => {
+    const links = $$(".js-dropdown-list > li > a");
+    // Get the container element of the menu
+    const menuContainer = document.querySelector(".js-dropdown-list");
+
+    links.forEach((link) => {
+        link.onclick = () => {
+            if (window.innerWidth > 991) return;
+
+            const item = link.closest("li");
+            const subMenu = item.querySelector(".sub-menu");
+
+            // close other submenu
+            const allItems = $$(".js-dropdown-list > li");
+            allItems.forEach((otherItem) => {
+                const otherSubMenu = otherItem.querySelector(".sub-menu");
+                if (otherItem !== item) {
+                    otherItem.classList.remove("menu-item-has-children--active");
+                    otherSubMenu.style.height = '0'; // Close other submenus
+                }
+            });
+
+            // Calculate the current submenu height
+            if (subMenu) {
+                // Get the actual height of the submenu.
+                const submenuHeight = subMenu.scrollHeight + "px";
+
+                // Toggle the current submenu state
+                if (item.classList.contains("menu-item-has-children--active")) {
+                    item.classList.remove("menu-item-has-children--active");
+                    subMenu.style.height = '0'; // close submenu
+                } else {
+                    item.classList.add("menu-item-has-children--active");
+                    subMenu.style.height = submenuHeight; // open submenu
+                }
+            }
+        };
+    });
+
+    // Close all submenus when clicking outside the menu
+    document.addEventListener("click", (e) => {
+        if (!menuContainer.contains(e.target)) {
+            const allItems = $$(".js-dropdown-list > li");
+            allItems.forEach(item => {
+                const subMenu = item.querySelector(".sub-menu");
+                item.classList.remove("menu-item-has-children--active");
+                subMenu.style.height = "0"; // Close all submenus.
+            });
+        }
+    });
+});
